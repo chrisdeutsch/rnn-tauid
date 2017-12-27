6dabc68cdbbe552da156eaa069cee2a94e41e4da
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2017-12-27 23:06:17 +0100

    MxAOD flattening script

--- scripts/flatten_mxaod.sh
+++ scripts/flatten_mxaod.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 MXAOD_DIR="MxAOD_v11/"
-OUTDIR="ntuples/"
-FLAGS="--rnnscore -n 100000"
+OUTDIR="ntuples_v11/"
+FLAGS="--rnnscore"
 
 echo "Input MxAOD directory: $MXAOD_DIR"
 echo "Flags for NtupleCreator: $FLAGS"
@@ -26,11 +26,26 @@ mv JZ2W/data-ntuple/*.root $OUTDIR/JZ2W.root
 rm -rf JZ2W/
 
 # JZ3W
-#RunNtupleCreator.py $MXAOD_DIR "*JZ3W*" -d JZ3W $FLAGS
-#mv JZ3W/data-ntuple/*.root $OUTDIR/JZ3W.root
-#rm -rf JZ3W/
+RunNtupleCreator.py $MXAOD_DIR "*JZ3W*" -d JZ3W $FLAGS
+mv JZ3W/data-ntuple/*.root $OUTDIR/JZ3W.root
+rm -rf JZ3W/
+
+# JZ4W
+RunNtupleCreator.py $MXAOD_DIR "*JZ4W*" -d JZ4W $FLAGS
+mv JZ4W/data-ntuple/*.root $OUTDIR/JZ4W.root
+rm -rf JZ4W/
+
+# JZ5W
+RunNtupleCreator.py $MXAOD_DIR "*JZ5W*" -d JZ5W $FLAGS
+mv JZ5W/data-ntuple/*.root $OUTDIR/JZ5W.root
+rm -rf JZ5W/
+
+# JZ6W
+RunNtupleCreator.py $MXAOD_DIR "*JZ6W*" -d JZ6W $FLAGS
+mv JZ6W/data-ntuple/*.root $OUTDIR/JZ6W.root
+rm -rf JZ6W/
 
 # Gammatautau
-#RunNtupleCreator.py $MXAOD_DIR "*Gammatautau*" -d Gammatautau --truth $FLAGS
-#mv Gammatautau/data-ntuple/*.root $OUTDIR/Gammatautau.root
-#rm -rf Gammatautau/
+RunNtupleCreator.py $MXAOD_DIR "*Gammatautau*" -d Gammatautau --truth $FLAGS
+mv Gammatautau/data-ntuple/*.root $OUTDIR/Gammatautau.root
+rm -rf Gammatautau/
